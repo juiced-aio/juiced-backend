0ea53af13d7c3bbd0fc0f6a81cb4b471e8641ede
Anthonyreeder <anthonyreeder123@gmail.com>
2021-06-03 08:14:56 +0100

    Monitor.go -> Setup page monitor
    Setup page monitor to function similar to that of amazon.
    Currently only supporting guest checkout but has room to add account later if this is wanted/needed.

--- juiced.sitescripts/hottopic/monitor.go
+++ juiced.sitescripts/hottopic/monitor.go
@@ -1,11 +1,16 @@
 package hottopic
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+
 	"backend.juicedbot.io/m/v2/juiced.infrastructure/common/entities"
 	"backend.juicedbot.io/m/v2/juiced.infrastructure/common/enums"
 	"backend.juicedbot.io/m/v2/juiced.infrastructure/common/events"
 	"backend.juicedbot.io/m/v2/juiced.sitescripts/base"
 	"backend.juicedbot.io/m/v2/juiced.sitescripts/util"
+	browser "github.com/eddycjy/fake-useragent"
 )
 
 // CreateHottopicMonitor takes a TaskGroup entity and turns it into a Hottopic Monitor
@@ -65,7 +70,7 @@ func (monitor *Monitor) RunMonitor() {
 		somethingInStock := false
 		switch monitor.PidWithInfo[pid].MonitorType {
 		case enums.SKUMonitor:
-			somethingInStock = monitor.isInStock(pid)
+			somethingInStock = monitor.StockMonitor(pid)
 		}
 		if somethingInStock {
 			needToStop := monitor.CheckForStop()
@@ -80,10 +85,63 @@ func (monitor *Monitor) RunMonitor() {
 	}
 }
 
-func (monitor *Monitor) isInStock(pid string) bool {
-	return true
+func (monitor *Monitor) StockMonitor(pid string) bool {
+	var client http.Client
+	client = monitor.PidWithInfo[pid].Client
+	resp, err := util.MakeRequest(&util.Request{
+		Client: client,
+		Method: "GET",
+		URL:    MonitorEndpoint + pid,
+		RawHeaders: [][2]string{
+			{"sec-ch-ua", `" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"`},
+			{"sec-ch-ua-mobile", "?0"},
+			{"upgrade-insecure-requests", "1"},
+			{"user-agent", browser.Chrome()},
+			{"accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"},
+			{"sec-fetch-site", "none"},
+			{"sec-fetch-mode", "navigate"},
+			{"sec-fetch-user", "?1"},
+			{"sec-fetch-dest", "document"},
+			{"accept-encoding", "gzip, deflate, br"},
+			{"accept-language", "en-US,en;q=0.9"},
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case 200:
+		test := monitor.StockInfo(resp, pid)
+		fmt.Println(test)
+		return test
+	case 404:
+		monitor.PublishEvent(enums.UnableToFindProduct, enums.MonitorUpdate)
+		return false
+	default:
+		fmt.Printf("Unkown Code:%v", resp.StatusCode)
+		return false
+	}
 }
 
+func (monitor *Monitor) StockInfo(resp *http.Response, pid string) bool {
+	//use soup to parse the HTML and get values we want
+	//
+	var priceStr string
+	price, _ := strconv.Atoi(priceStr)
+	inBudget := monitor.PidWithInfo[pid].MaxPrice > price
+	if inBudget {
+		monitor.EventInfo = events.HotTopicSingleStockData{
+			PID:         pid,
+			MonitorType: enums.SKUMonitor,
+			//anymore data you want to scrape put here such as item name, size, color etc.
+		}
+	}
+
+	return inBudget
+}
 func (monitor *Monitor) SendToTasks() {
 	data := events.HottopicStockData{
 		InStock: []events.HotTopicSingleStockData{monitor.EventInfo},
