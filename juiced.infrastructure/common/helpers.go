137dbda746c2c2cfc3ca701d4ab9adc5f2541ee7
IHaveNothingg <80022991+IHaveNothingg@users.noreply.github.com>
2021-07-01 15:46:54 -0700

    Values will never be null and we can also interchange the data type now

--- juiced.infrastructure/common/helpers.go
+++ juiced.infrastructure/common/helpers.go
@@ -75,6 +75,7 @@ var database *sqlx.DB
 // InitDatabase initializes the database singleton
 func InitDatabase() error {
 	var err error
+
 	configPath := configdir.LocalConfig("juiced")
 	err = configdir.MakePath(configPath)
 	if err != nil {
@@ -90,19 +91,24 @@ func InitDatabase() error {
 		_, err = database.Exec(schema)
 		tableName, _ := FindInString(schema, "EXISTS ", " \\(")
 		missing, extra := CompareColumns(ParseColumns(schema), GetCurrentColumns(schema))
+		for i := range extra {
+			extraSplit := strings.Split(extra[i], "|")
+			_, err = database.Exec(fmt.Sprintf("ALTER TABLE %v DROP COLUMN %v", tableName, extraSplit[0]))
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+		}
+
 		for i := range missing {
 			missingSplit := strings.Split(missing[i], "|")
-			_, err = database.Exec(fmt.Sprintf("ALTER TABLE %v ADD COLUMN %v %v", tableName, missingSplit[0], missingSplit[1]))
+			_, err = database.Exec(fmt.Sprintf("ALTER TABLE %v ADD COLUMN %v %v DEFAULT ''", tableName, missingSplit[0], missingSplit[1]))
 			if err != nil {
+				fmt.Println(err)
 				return err
 			}
 		}
-		for i := range extra {
-			_, err = database.Exec(fmt.Sprintf("ALTER TABLE %v DROP COLUMN %v", tableName, extra[i]))
-			if err != nil {
-				return err
-			}
-		}
+
 	}
 
 	return err
@@ -144,7 +150,7 @@ func GetCurrentColumns(schema string) (columnNames []string) {
 
 	for rows.Next() {
 		column, _ := rows.SliceScan()
-		columnNames = append(columnNames, column[1].(string))
+		columnNames = append(columnNames, column[1].(string)+"|"+column[2].(string))
 	}
 	return
 }
@@ -152,26 +158,33 @@ func GetCurrentColumns(schema string) (columnNames []string) {
 func CompareColumns(x []string, y []string) ([]string, []string) {
 	var missing []string
 	var extra []string
-	for i := range x {
+
+	for _, extraColumn1 := range x {
 		var inside bool
-		for _, name := range y {
-			if name == strings.Split(x[i], "|")[0] {
+		extraColumnSplit1 := strings.Split(extraColumn1, "|")
+		for _, extraColumn2 := range y {
+			extraColumnSplit2 := strings.Split(extraColumn2, "|")
+			if extraColumnSplit2[0] == extraColumnSplit1[0] && extraColumnSplit2[1] == extraColumnSplit1[1] {
 				inside = true
 			}
 		}
 		if !inside {
-			missing = append(missing, x[i])
+			missing = append(missing, extraColumn1)
 		}
 	}
-	for i := range y {
+
+	for _, missingColumn1 := range y {
 		var inside bool
-		for _, name := range x {
-			if strings.Split(name, "|")[0] == y[i] {
+		missingColumnSplit1 := strings.Split(missingColumn1, "|")
+		for _, missingColumn2 := range x {
+
+			missingColumnSplit2 := strings.Split(missingColumn2, "|")
+			if missingColumnSplit2[0] == missingColumnSplit1[0] && missingColumnSplit2[1] == missingColumnSplit1[1] {
 				inside = true
 			}
 		}
 		if !inside {
-			extra = append(extra, y[i])
+			extra = append(extra, missingColumn1)
 		}
 	}
 
