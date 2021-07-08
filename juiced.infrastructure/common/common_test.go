66139da5f8485ebcd6046e02cbb90262b1cbf38e
IHaveNothingg <andersonrector@gmail.com>
2021-07-07 17:10:52 -0700

    Some tests

---
+++ juiced.infrastructure/common/common_test.go
@@ -0,0 +1,45 @@
+package common
+
+import (
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func TestRandString(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{args: args{n: 16}, want: 16},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := len(RandString(tt.args.n)); got != tt.want {
+				t.Errorf("RandString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRandID(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{args: args{n: 16}, want: 16},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := len(RandID(tt.args.n)); got != tt.want {
+				t.Errorf("RandID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
