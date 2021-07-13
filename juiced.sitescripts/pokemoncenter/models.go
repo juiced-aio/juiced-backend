ccaac46aadec7eddc66470bfbaca236a5ec18130
Anthonyreeder <anthonyreeder123@gmail.com>
2021-07-13 22:41:05 +0100

    Models - WIP
    Models for monitor. ->> More work WIP

--- juiced.sitescripts/pokemoncenter/models.go
+++ juiced.sitescripts/pokemoncenter/models.go
@@ -1,7 +1,7 @@
 package pokemoncenter
 
 import (
-	"backend.juicedbot.io/juiced.infrastructure/common/enums"
+	"backend.juicedbot.io/juiced.infrastructure/common/entities"
 	"backend.juicedbot.io/juiced.sitescripts/base"
 )
 
@@ -9,16 +9,59 @@ import (
 const (
 	BaseEndpoint = "https://www.pokemoncenter.com/"
 
-	MonitorEndpoint = ""
+	MonitorEndpoint = "https://www.pokemoncenter.com/product/%s"
 )
 
 // Monitor info
 type Monitor struct {
-	Monitor     base.Monitor
-	MonitorType enums.MonitorType
+	Monitor         base.Monitor
+	SKUsSentToTask  []string
+	RunningMonitors []string
+	OutOfStockSKUs  []string
+	SKUs            []string
+	InStock         []PokemonCenterInStockData
+	SKUWithInfo     map[string]entities.PokemonCenterSingleMonitorInfo
+}
+
+type PokemonCenterInStockData struct {
+	SKU           string
+	Price         float64
+	ItemName      string
+	AddToCartForm string
+	ImageURL      string
+}
+
+type MonitorResponse struct {
+	Props struct {
+		IsServer     bool
+		InitialState struct {
+			Product struct {
+				AddToCartForm string
+				Availability  string
+				Images        struct {
+					Original string
+				}
+				ListPrice struct {
+					Amount float64
+				}
+				Name string
+			}
+		}
+		InitialProps struct {
+		}
+	}
 }
 
 // Task info
 type Task struct {
-	Task base.Task
+	Task         base.Task
+	CheckoutInfo CheckoutInfo
+}
+
+type CheckoutInfo struct {
+	AddToCartForm string
+	AuthId        string
+	ImageURL      string
+	Price         int
+	ItemName      string
 }
