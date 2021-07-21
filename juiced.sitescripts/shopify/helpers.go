b013fd85837273b8b0a1172ae4d675f4e8c1cdfa
IHaveNothingg <andersonrector@gmail.com>
2021-07-21 02:11:37 -0700

    One more card type
    All the others are 16 long

--- juiced.sitescripts/shopify/helpers.go
+++ juiced.sitescripts/shopify/helpers.go
@@ -16,6 +16,8 @@ func SplitCard(card string, cardType string) string {
 	switch cardType {
 	case "AMEX":
 		cardSplit = strings.Join([]string{card[:4], card[4:10], card[10:15]}, " ")
+	case "Diners":
+		cardSplit = strings.Join([]string{card[:4], card[4:10], card[10:14]}, " ")
 	default:
 		cardSplit = strings.Join([]string{card[:4], card[4:8], card[8:12], card[12:16]}, " ")
 	}
