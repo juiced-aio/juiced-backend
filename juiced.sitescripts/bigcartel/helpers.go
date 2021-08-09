dd2e3a26c0dda26acfacf23a0141a793bfb0ac1c
Anthonyreeder <anthonyreeder123@gmail.com>
2021-08-09 16:30:03 +0100

    Add checkout (discord webhook)
    Add checkout (discord webhook)

--- juiced.sitescripts/bigcartel/helpers.go
+++ juiced.sitescripts/bigcartel/helpers.go
@@ -5,7 +5,71 @@ import (
 	"time"
 
 	"backend.juicedbot.io/juiced.infrastructure/common/enums"
+	sec "backend.juicedbot.io/juiced.security/auth/util"
+	"backend.juicedbot.io/juiced.sitescripts/util"
 )
+
+// Creates a embed for the DiscordWebhook function
+func (task *Task) CreateBigCartelEmbed(status enums.OrderStatus, imageURL string) []sec.DiscordEmbed {
+	embeds := []sec.DiscordEmbed{
+		{
+			Fields: []sec.DiscordField{
+				{
+					Name:   "Site:",
+					Value:  task.BigCartelRetailer,
+					Inline: true,
+				},
+				{
+					Name:   "Price:",
+					Value:  "$" + fmt.Sprint(task.InStockData.ItemPrice),
+					Inline: true,
+				},
+				{
+					Name:   "Product SKU:",
+					Value:  fmt.Sprintf("[%v](%v%v)", task.SiteURL, task.InStockData.Sku, task.InStockData.ImageURL),
+					Inline: true,
+				},
+				{
+					Name:  "Product Name:",
+					Value: task.InStockData.ItemName,
+				},
+				{
+					Name:  "Proxy:",
+					Value: "||" + " " + util.ProxyCleaner(task.Task.Proxy) + " " + "||",
+				},
+			},
+			Footer: sec.DiscordFooter{
+				Text:    "Juiced AIO",
+				IconURL: "https://media.discordapp.net/attachments/849430464036077598/855979506204278804/Icon_1.png?width=128&height=128",
+			},
+			Timestamp: time.Now(),
+		},
+	}
+
+	switch status {
+	case enums.OrderStatusSuccess:
+		embeds[0].Title = ":tangerine: Checkout! :tangerine:"
+		embeds[0].Color = 16742912
+		embeds[0].Thumbnail = sec.DiscordThumbnail{
+			URL: imageURL,
+		}
+	case enums.OrderStatusDeclined:
+		embeds[0].Title = ":lemon: Card Declined :lemon:"
+		embeds[0].Color = 16766464
+		embeds[0].Thumbnail = sec.DiscordThumbnail{
+			URL: imageURL,
+		}
+	case enums.OrderStatusFailed:
+		embeds[0].Title = ":apple: Failed to Place Order :apple:"
+		embeds[0].Color = 14495044
+		embeds[0].Thumbnail = sec.DiscordThumbnail{
+			URL: imageURL,
+		}
+	}
+
+	return embeds
+
+}
 
 //Improves readability on RunTask
 func (task *Task) RunUntilSuccessful(runTaskResult bool, status string) (bool, bool) {
