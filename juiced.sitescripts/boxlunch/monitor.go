fd921a19cbd968edb0621d4fb21b552487d4c107
IHaveNothingg <80022991+Humphryyy@users.noreply.github.com>
2021-08-24 21:26:35 -0700

    Displaying item info on OOS, thought there were some Disney problems for a second and spent a minute checking it out

--- juiced.sitescripts/boxlunch/monitor.go
+++ juiced.sitescripts/boxlunch/monitor.go
@@ -390,11 +390,14 @@ func (monitor *Monitor) GetVariationInfo(body, pid string) ([]BoxlunchSizeInfo, 
 		return sizes, colors, stockData, productNameHeader.Error
 	}
 	productName := productNameHeader.Text()
+	stockData.ProductName = productName
+
 	productImage := doc.Find("img", "class", "productdetail__image-active-each")
 	if productImage.Error != nil {
 		return sizes, colors, stockData, productImage.Error
 	}
 	imageURL := productImage.Attrs()["src"]
+	stockData.ImageURL = imageURL
 
 	hasVariations := false
 	// This element will only exist if the product has no size/color variations and the item is in stock
