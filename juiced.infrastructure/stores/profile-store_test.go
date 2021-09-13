6e7bf00f3984da14112fbf59cece6f7dc2211f0a
Silent Dev <silent@juicedbot.io>
2021-09-13 13:43:12 -0400

    Complete CreateProfile tests

--- juiced.infrastructure/stores/profile-store_test.go
+++ juiced.infrastructure/stores/profile-store_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"backend.juicedbot.io/juiced.infrastructure/database"
 	"backend.juicedbot.io/juiced.infrastructure/entities"
 	"backend.juicedbot.io/juiced.infrastructure/stores"
 )
@@ -44,9 +45,17 @@ func TestProfileStore(t *testing.T) {
 		BillingAddress:  address1,
 		CreditCard:      card1,
 	}
+	profile3 := entities.Profile{
+		Name:            "3",
+		Email:           "email@email.com",
+		PhoneNumber:     "1231231234",
+		ShippingAddress: address1,
+		BillingAddress:  address1,
+		CreditCard:      card1,
+	}
 	var profile1Ptr *entities.Profile
 	var profile2Ptr *entities.Profile
-	// var profile3Ptr *entities.Profile
+	var profile3Ptr *entities.Profile
 
 	t.Run("GetAllProfiles", func(t *testing.T) {
 		t.Run("GetAllProfiles returns empty array upon first startup", func(t *testing.T) {
@@ -207,16 +216,235 @@ func TestProfileStore(t *testing.T) {
 
 	t.Run("CreateProfile", func(t *testing.T) {
 		t.Run("CreateProfile returns ProfileAlreadyExistsError if a Profile with the given name already exists", func(t *testing.T) {
-
+			_, err := stores.CreateProfile(profile1)
+			if err == nil {
+				t.Fatal("CreateProfile should have failed due to duplicate name but succeeded\n", err)
+			}
+			if _, ok := err.(*stores.ProfileAlreadyExistsError); !ok {
+				t.Fatalf("CreateProfile did not return a ProfileAlreadyExistsError (actual error: %v)\n", err)
+			}
 		})
 		t.Run("CreateProfile returns correct Profile", func(t *testing.T) {
-
+			profile3Ptr, err = stores.CreateProfile(profile3)
+			if err != nil {
+				t.Fatalf("CreateProfile failed: %v\n", err)
+			}
+			if profile3Ptr.Name != profile3.Name {
+				t.Fatalf("Created Profile does not have same name (%s) as stored profile3 (%s)\n", profile3Ptr.Name, profile3.Name)
+			}
+			if profile3Ptr.Email != profile3.Email {
+				t.Fatalf("Created Profile does not have same email (%s) as stored profile3 (%s)\n", profile3Ptr.Email, profile3.Email)
+			}
+			if profile3Ptr.PhoneNumber != profile3.PhoneNumber {
+				t.Fatalf("Created Profile does not have same phone number (%s) as stored profile3 (%s)\n", profile3Ptr.PhoneNumber, profile3.PhoneNumber)
+			}
+			if profile3Ptr.ShippingAddress.FirstName != profile3.ShippingAddress.FirstName {
+				t.Fatalf("Created Profile does not have same ShippingAddress.FirstName (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.FirstName, profile3.ShippingAddress.FirstName)
+			}
+			if profile3Ptr.ShippingAddress.LastName != profile3.ShippingAddress.LastName {
+				t.Fatalf("Created Profile does not have same ShippingAddress.LastName (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.LastName, profile3.ShippingAddress.FirstName)
+			}
+			if profile3Ptr.ShippingAddress.Address1 != profile3.ShippingAddress.Address1 {
+				t.Fatalf("Created Profile does not have same ShippingAddress.Address1 (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.Address1, profile3.ShippingAddress.Address1)
+			}
+			if profile3Ptr.ShippingAddress.Address2 != profile3.ShippingAddress.Address2 {
+				t.Fatalf("Created Profile does not have same ShippingAddress.Address2 (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.Address2, profile3.ShippingAddress.Address2)
+			}
+			if profile3Ptr.ShippingAddress.City != profile3.ShippingAddress.City {
+				t.Fatalf("Created Profile does not have same ShippingAddress.City (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.City, profile3.ShippingAddress.City)
+			}
+			if profile3Ptr.ShippingAddress.ZipCode != profile3.ShippingAddress.ZipCode {
+				t.Fatalf("Created Profile does not have same ShippingAddress.ZipCode (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.ZipCode, profile3.ShippingAddress.ZipCode)
+			}
+			if profile3Ptr.ShippingAddress.StateCode != profile3.ShippingAddress.StateCode {
+				t.Fatalf("Created Profile does not have same ShippingAddress.StateCode (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.StateCode, profile3.ShippingAddress.StateCode)
+			}
+			if profile3Ptr.ShippingAddress.CountryCode != profile3.ShippingAddress.CountryCode {
+				t.Fatalf("Created Profile does not have same ShippingAddress.CountryCode (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.CountryCode, profile3.ShippingAddress.CountryCode)
+			}
+			if profile3Ptr.BillingAddress.FirstName != profile3.BillingAddress.FirstName {
+				t.Fatalf("Created Profile does not have same BillingAddress.FirstName (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.FirstName, profile3.BillingAddress.FirstName)
+			}
+			if profile3Ptr.BillingAddress.LastName != profile3.BillingAddress.LastName {
+				t.Fatalf("Created Profile does not have same BillingAddress.LastName (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.LastName, profile3.BillingAddress.FirstName)
+			}
+			if profile3Ptr.BillingAddress.Address1 != profile3.BillingAddress.Address1 {
+				t.Fatalf("Created Profile does not have same BillingAddress.Address1 (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.Address1, profile3.BillingAddress.Address1)
+			}
+			if profile3Ptr.BillingAddress.Address2 != profile3.BillingAddress.Address2 {
+				t.Fatalf("Created Profile does not have same BillingAddress.Address2 (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.Address2, profile3.BillingAddress.Address2)
+			}
+			if profile3Ptr.BillingAddress.City != profile3.BillingAddress.City {
+				t.Fatalf("Created Profile does not have same BillingAddress.City (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.City, profile3.BillingAddress.City)
+			}
+			if profile3Ptr.BillingAddress.ZipCode != profile3.BillingAddress.ZipCode {
+				t.Fatalf("Created Profile does not have same BillingAddress.ZipCode (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.ZipCode, profile3.BillingAddress.ZipCode)
+			}
+			if profile3Ptr.BillingAddress.StateCode != profile3.BillingAddress.StateCode {
+				t.Fatalf("Created Profile does not have same BillingAddress.StateCode (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.StateCode, profile3.BillingAddress.StateCode)
+			}
+			if profile3Ptr.BillingAddress.CountryCode != profile3.BillingAddress.CountryCode {
+				t.Fatalf("Created Profile does not have same BillingAddress.CountryCode (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.CountryCode, profile3.BillingAddress.CountryCode)
+			}
+			if profile3Ptr.CreditCard.CardholderName != profile3.CreditCard.CardholderName {
+				t.Fatalf("Created Profile does not have same CreditCard.CardholderName (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CardholderName, profile3.CreditCard.CardholderName)
+			}
+			if profile3Ptr.CreditCard.CardNumber != profile3.CreditCard.CardNumber {
+				t.Fatalf("Created Profile does not have same CreditCard.CardNumber (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CardNumber, profile3.CreditCard.CardNumber)
+			}
+			if profile3Ptr.CreditCard.ExpMonth != profile3.CreditCard.ExpMonth {
+				t.Fatalf("Created Profile does not have same CreditCard.ExpMonth (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.ExpMonth, profile3.CreditCard.ExpMonth)
+			}
+			if profile3Ptr.CreditCard.ExpYear != profile3.CreditCard.ExpYear {
+				t.Fatalf("Created Profile does not have same CreditCard.ExpYear (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.ExpYear, profile3.CreditCard.ExpYear)
+			}
+			if profile3Ptr.CreditCard.CVV != profile3.CreditCard.CVV {
+				t.Fatalf("Created Profile does not have same CreditCard.CVV (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CVV, profile3.CreditCard.CVV)
+			}
+			if profile3Ptr.CreditCard.CardType != profile3.CreditCard.CardType {
+				t.Fatalf("Created Profile does not have same CreditCard.CardType (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CardType, profile3.CreditCard.CardType)
+			}
+			if profile3Ptr.ID == "" {
+				t.Fatal("Created Profile has an empty ID\n")
+			}
+			if profile3Ptr.CreationDate == 0 {
+				t.Fatal("Created Profile has CreationDate = 0\n")
+			}
+			if profile3Ptr.ShippingAddress.ID == "" {
+				t.Fatal("Created Profile.ShippingAddress has an empty ID\n")
+			}
+			if profile3Ptr.ShippingAddress.ProfileID != profile3Ptr.ID {
+				t.Fatalf("Created Profile.ShippingAddress has different ProfileID (%s) than created Profile's ID (%s)\n", profile3Ptr.ShippingAddress.ProfileID, profile3Ptr.ID)
+			}
+			if profile3Ptr.BillingAddress.ID == "" {
+				t.Fatal("Created Profile.BillingAddress has an empty ID\n")
+			}
+			if profile3Ptr.BillingAddress.ProfileID != profile3Ptr.ID {
+				t.Fatalf("Created Profile.BillingAddress has different ProfileID (%s) than created Profile's ID (%s)\n", profile3Ptr.BillingAddress.ProfileID, profile3Ptr.ID)
+			}
+			if profile3Ptr.CreditCard.ID == "" {
+				t.Fatal("Created Profile.CreditCard has an empty ID\n")
+			}
+			if profile3Ptr.CreditCard.ProfileID != profile3Ptr.ID {
+				t.Fatalf("Created Profile.CreditCard has different ProfileID (%s) than created Profile's ID (%s)\n", profile3Ptr.CreditCard.ProfileID, profile3Ptr.ID)
+			}
 		})
 		t.Run("CreateProfile adds the Profile to the profileStore", func(t *testing.T) {
-
+			profile3Store, err := stores.GetProfile(profile3Ptr.ID)
+			if err != nil {
+				t.Fatalf("stores.GetProfile failed: %v\n", err)
+			}
+			if profile3Store != profile3Ptr {
+				t.Fatalf("Profile returned by GetProfile does not have same pointer (%p) as stored profile3Ptr (%p)\n", profile3Store, profile3Ptr)
+			}
 		})
 		t.Run("CreateProfile adds the Profile to the database", func(t *testing.T) {
-
+			profile3Database, err := database.GetProfile(profile3Ptr.ID)
+			if err != nil {
+				t.Fatalf("database.GetProfile failed: %v\n", err)
+			}
+			if profile3Database.ID != profile3Ptr.ID {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ID (%s) as stored profile3Ptr (%s)\n", profile3Database.ID, profile3Ptr.ID)
+			}
+			if profile3Ptr.Name != profile3.Name {
+				t.Fatalf("Profile returned by database.GetProfile does not have same name (%s) as stored profile3 (%s)\n", profile3Ptr.Name, profile3.Name)
+			}
+			if profile3Ptr.Email != profile3.Email {
+				t.Fatalf("Profile returned by database.GetProfile does not have same email (%s) as stored profile3 (%s)\n", profile3Ptr.Email, profile3.Email)
+			}
+			if profile3Ptr.PhoneNumber != profile3.PhoneNumber {
+				t.Fatalf("Profile returned by database.GetProfile does not have same phone number (%s) as stored profile3 (%s)\n", profile3Ptr.PhoneNumber, profile3.PhoneNumber)
+			}
+			if profile3Ptr.ShippingAddress.FirstName != profile3.ShippingAddress.FirstName {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.FirstName (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.FirstName, profile3.ShippingAddress.FirstName)
+			}
+			if profile3Ptr.ShippingAddress.LastName != profile3.ShippingAddress.LastName {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.LastName (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.LastName, profile3.ShippingAddress.FirstName)
+			}
+			if profile3Ptr.ShippingAddress.Address1 != profile3.ShippingAddress.Address1 {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.Address1 (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.Address1, profile3.ShippingAddress.Address1)
+			}
+			if profile3Ptr.ShippingAddress.Address2 != profile3.ShippingAddress.Address2 {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.Address2 (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.Address2, profile3.ShippingAddress.Address2)
+			}
+			if profile3Ptr.ShippingAddress.City != profile3.ShippingAddress.City {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.City (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.City, profile3.ShippingAddress.City)
+			}
+			if profile3Ptr.ShippingAddress.ZipCode != profile3.ShippingAddress.ZipCode {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.ZipCode (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.ZipCode, profile3.ShippingAddress.ZipCode)
+			}
+			if profile3Ptr.ShippingAddress.StateCode != profile3.ShippingAddress.StateCode {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.StateCode (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.StateCode, profile3.ShippingAddress.StateCode)
+			}
+			if profile3Ptr.ShippingAddress.CountryCode != profile3.ShippingAddress.CountryCode {
+				t.Fatalf("Profile returned by database.GetProfile does not have same ShippingAddress.CountryCode (%s) as stored profile3 (%s)\n", profile3Ptr.ShippingAddress.CountryCode, profile3.ShippingAddress.CountryCode)
+			}
+			if profile3Ptr.BillingAddress.FirstName != profile3.BillingAddress.FirstName {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.FirstName (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.FirstName, profile3.BillingAddress.FirstName)
+			}
+			if profile3Ptr.BillingAddress.LastName != profile3.BillingAddress.LastName {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.LastName (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.LastName, profile3.BillingAddress.FirstName)
+			}
+			if profile3Ptr.BillingAddress.Address1 != profile3.BillingAddress.Address1 {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.Address1 (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.Address1, profile3.BillingAddress.Address1)
+			}
+			if profile3Ptr.BillingAddress.Address2 != profile3.BillingAddress.Address2 {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.Address2 (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.Address2, profile3.BillingAddress.Address2)
+			}
+			if profile3Ptr.BillingAddress.City != profile3.BillingAddress.City {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.City (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.City, profile3.BillingAddress.City)
+			}
+			if profile3Ptr.BillingAddress.ZipCode != profile3.BillingAddress.ZipCode {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.ZipCode (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.ZipCode, profile3.BillingAddress.ZipCode)
+			}
+			if profile3Ptr.BillingAddress.StateCode != profile3.BillingAddress.StateCode {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.StateCode (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.StateCode, profile3.BillingAddress.StateCode)
+			}
+			if profile3Ptr.BillingAddress.CountryCode != profile3.BillingAddress.CountryCode {
+				t.Fatalf("Profile returned by database.GetProfile does not have same BillingAddress.CountryCode (%s) as stored profile3 (%s)\n", profile3Ptr.BillingAddress.CountryCode, profile3.BillingAddress.CountryCode)
+			}
+			if profile3Ptr.CreditCard.CardholderName != profile3.CreditCard.CardholderName {
+				t.Fatalf("Profile returned by database.GetProfile does not have same CreditCard.CardholderName (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CardholderName, profile3.CreditCard.CardholderName)
+			}
+			if profile3Ptr.CreditCard.CardNumber != profile3.CreditCard.CardNumber {
+				t.Fatalf("Profile returned by database.GetProfile does not have same CreditCard.CardNumber (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CardNumber, profile3.CreditCard.CardNumber)
+			}
+			if profile3Ptr.CreditCard.ExpMonth != profile3.CreditCard.ExpMonth {
+				t.Fatalf("Profile returned by database.GetProfile does not have same CreditCard.ExpMonth (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.ExpMonth, profile3.CreditCard.ExpMonth)
+			}
+			if profile3Ptr.CreditCard.ExpYear != profile3.CreditCard.ExpYear {
+				t.Fatalf("Profile returned by database.GetProfile does not have same CreditCard.ExpYear (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.ExpYear, profile3.CreditCard.ExpYear)
+			}
+			if profile3Ptr.CreditCard.CVV != profile3.CreditCard.CVV {
+				t.Fatalf("Profile returned by database.GetProfile does not have same CreditCard.CVV (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CVV, profile3.CreditCard.CVV)
+			}
+			if profile3Ptr.CreditCard.CardType != profile3.CreditCard.CardType {
+				t.Fatalf("Profile returned by database.GetProfile does not have same CreditCard.CardType (%s) as stored profile3 (%s)\n", profile3Ptr.CreditCard.CardType, profile3.CreditCard.CardType)
+			}
+			if profile3Ptr.ID == "" {
+				t.Fatal("Profile returned by database.GetProfile has an empty ID\n")
+			}
+			if profile3Ptr.CreationDate == 0 {
+				t.Fatal("Profile returned by database.GetProfile has CreationDate = 0\n")
+			}
+			if profile3Ptr.ShippingAddress.ID == "" {
+				t.Fatal("Profile returned by database.GetProfile.ShippingAddress has an empty ID\n")
+			}
+			if profile3Ptr.ShippingAddress.ProfileID != profile3Ptr.ID {
+				t.Fatalf("Profile returned by database.GetProfile.ShippingAddress has different ProfileID (%s) than created Profile's ID (%s)\n", profile3Ptr.ShippingAddress.ProfileID, profile3Ptr.ID)
+			}
+			if profile3Ptr.BillingAddress.ID == "" {
+				t.Fatal("Profile returned by database.GetProfile.BillingAddress has an empty ID\n")
+			}
+			if profile3Ptr.BillingAddress.ProfileID != profile3Ptr.ID {
+				t.Fatalf("Profile returned by database.GetProfile.BillingAddress has different ProfileID (%s) than created Profile's ID (%s)\n", profile3Ptr.BillingAddress.ProfileID, profile3Ptr.ID)
+			}
+			if profile3Ptr.CreditCard.ID == "" {
+				t.Fatal("Profile returned by database.GetProfile.CreditCard has an empty ID\n")
+			}
+			if profile3Ptr.CreditCard.ProfileID != profile3Ptr.ID {
+				t.Fatalf("Profile returned by database.GetProfile.CreditCard has different ProfileID (%s) than created Profile's ID (%s)\n", profile3Ptr.CreditCard.ProfileID, profile3Ptr.ID)
+			}
 		})
 	})
 
