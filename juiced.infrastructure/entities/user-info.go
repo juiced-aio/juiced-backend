d47b1df15b678b3ac330466a7f2f38a1e3da4cb1
Silent Dev <silent@juicedbot.io>
2021-09-16 12:42:05 -0400

    Update user info entity

--- juiced.infrastructure/entities/user-info.go
+++ juiced.infrastructure/entities/user-info.go
@@ -1,13 +1,5 @@
 package entities
 
-type Channel = string
-
-const (
-	ALPHA   Channel = "Alpha"
-	BETA    Channel = "Beta"
-	Release Channel = "Release"
-)
-
 type UserInfo struct {
 	Email            string `json:"email" db:"email"`
 	UserID           string `json:"userID" db:"userID"`
@@ -20,8 +12,8 @@ type UserInfo struct {
 	RefreshToken     string `json:"refreshToken" db:"refreshToken"`
 	ExpiresAt        int64  `json:"expiresAt" db:"expiresAt"`
 
-	Channel         Channel `json:"channel" db:"channel"`
-	BackendVersion  string  `json:"backendVersion" db:"backendVersion"`
-	EncryptionKey   string  `json:"encryptionKey"`
-	FrontendVersion string  `json:"frontendVersion"`
+	Channel         string `json:"channel" db:"channel"`
+	BackendVersion  string `json:"backendVersion" db:"backendVersion"`
+	EncryptionKey   string `json:"encryptionKey"`
+	FrontendVersion string `json:"frontendVersion"`
 }
